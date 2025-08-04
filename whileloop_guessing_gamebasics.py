9025747b3ac4d10e0755fd54f11228289424e5b4
ElaRose ;) <anamikakumarishaw2004@gmail.com>
2025-08-04 15:36:02 +0530

    Add files via upload
    🔁 Added while loop guessing game with user input

---
+++ whileloop_guessing_gamebasics.py
@@ -0,0 +1,8 @@
+secret=7
+guess=0
+ 
+while guess!=secret:
+	guess=int(input("guess the number(1-10): "));
+	if guess!=secret:
+		print("Oops! wrong try again:)");
+print("congrats! you guess it right..");		
